01a0063e8057468f956a6fec324dc34a659692b4
J. Taylor Allen <j.taylorallen@yahoo.com>
2021-09-19 16:47:50 -0500

    honestly lost from last com logo, adopt, README, css

---
+++ adoption/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+init() //calls when page starts up leave at bottom
