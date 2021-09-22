8d61eb70b64816af73e233f5da0ddd7f1a418573
J. Taylor Allen <j.taylorallen@yahoo.com>
2021-09-21 19:53:39 -0500

    all or nothing

--- adoption/js/init.js
+++ adoption/js/init.js
@@ -4,7 +4,23 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-init() //calls when page starts up leave at bottom
+// init() //calls when page starts up leave at bottom
+
+// <!-- <script>
+//    $(document).ready(function(){
+//     $('select').formSelect();
+//     }); -->
+
+//     $(document).ready(function(){
+//       $('.modal').modal();
+//     });
+
+      document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems);
+    });
